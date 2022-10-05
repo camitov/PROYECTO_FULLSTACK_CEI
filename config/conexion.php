a8954047b0677b0954f27cbe5eea58f34d4f69f3
Camilo Andres Tovar B <camilotovar4200@gmail.com>
2022-10-04 20:55:00 -0500

    10/04/2022 Se conecta la base de datos con MySQL

--- config/conexion.php
+++ config/conexion.php
@@ -1,15 +1,23 @@
 <?php
-class Conectar {
-    protected $DB;
 
-    protected function Conexion(){
-        try{
+/* Se crea la conección con la base de datos */
+class Conectar
+{
+    protected $DB;
+    protected function Conexion()
+    {
+        try {
             $conectar = $this->DB = new PDO("mysql:local=localhost;dbname=citas", "root", "");
             return $conectar;
-        }catch(Exception $e){
-            print "¡Error BD!: " . $e ->getMessage() . "<br/>";
-            die() 
+        }
+        /* Si hay error en la conexión saca un mensaje */ catch (Exception $e) {
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
         }
     }
-} 
-?>
+/* Para no tener problema con las minúsculas ni mayusculas */
+    public function set_names(){
+        return $this->DB->query("SET NAMES 'utf8'");
+    }
+
+}
